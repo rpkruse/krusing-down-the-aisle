8ac000ce3d4463ecc70802ae558fdc833aaf9146
rpkruse <rpkruse@ksu.edu>
2018-11-21 14:09:29 -0600

    Started homepage skele

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './global/shared-styles.css']
 })
 export class AppComponent {
   title = 'krusing-down-the-aisle';
