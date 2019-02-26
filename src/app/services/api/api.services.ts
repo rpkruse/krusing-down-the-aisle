a2fec26836217d6d6c5d4e6b273fef20ad4d731e
rkruse <rkruse@xpanxion.com>
2019-02-26 10:50:46 -0600

    Clean up, added pooch

--- src/app/services/api/api.services.ts
+++ src/app/services/api/api.services.ts
@@ -1,5 +1,5 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
