aaa0aa5562d5ac0032ab8e7227d5d06d278697d2
rkruse <rkruse@xpanxion.com>
2019-02-28 15:42:02 -0600

    Added loading check to wedding party page

--- src/app/wedding-party/wedding-party.component.ts
+++ src/app/wedding-party/wedding-party.component.ts
@@ -9,24 +9,36 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./wedding-party.component.css', '../global/shared-styles.css']
 })
 export class WeddingPartyComponent implements OnInit {
+  private pullCount: number = 0;
+
   bridalParty:IWeddingParty[] = [];
   groomsParty: IWeddingParty[] = [];
+
+  loading: boolean = true;
 
   constructor(private _apiService: ApiService) { }
 
   ngOnInit() {
-    let s, j: Subscription;
+    let s: Subscription, j: Subscription;
 
     s = this._apiService.getAllEntities<IWeddingParty>('WeddingParty/BridalParty').subscribe(
       d => this.bridalParty = d,
       err => console.log('unable to get bridal party', err),
-      () => s.unsubscribe()
+      () => {
+        s.unsubscribe();
+        this.pullCount++;
+        this.loading = this.pullCount != 2;
+      }
     );
 
     j = this._apiService.getAllEntities<IWeddingParty>('WeddingParty/GroomsParty').subscribe(
       d => this.groomsParty = d,
       err => console.log('unable to get grooms party', err),
-      () => j.unsubscribe()
+      () => {
+        j.unsubscribe();
+        this.pullCount++;
+        this.loading = this.pullCount != 2;
+      }
     );
   }
 
