95538b93eedd0a34a39790a37ee374360ed7c541
rkruse <rkruse@xpanxion.com>
2019-02-28 14:55:20 -0600

    Started photos page skeleton

--- src/app/photos/photos.component.ts
+++ src/app/photos/photos.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-photos',
   templateUrl: './photos.component.html',
-  styleUrls: ['./photos.component.css']
+  styleUrls: ['./photos.component.css', '../global/shared-styles.css']
 })
 export class PhotosComponent implements OnInit {
+  
+  //This is temp
+  images: string[] = [
+    "assets/pictures/food/chicken.png",
+    "assets/pictures/food/fish.png",
+    "assets/pictures/food/salad.png",
+    "assets/pictures/food/steak.png"
+  ];
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
