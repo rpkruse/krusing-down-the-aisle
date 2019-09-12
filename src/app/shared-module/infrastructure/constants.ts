a22fb60533a37d5ec7a023affc38dd7655dcf0e5
rpkruse <rpkruse@ksu.edu>
2019-09-12 16:54:51 -0500

    Minor code clean up

--- src/app/shared-module/infrastructure/constants.ts
+++ src/app/shared-module/infrastructure/constants.ts
@@ -1,12 +1,12 @@
 export class Constants {
-	static uiRoutes = {
-		empty: '',
-		home: 'home',
-		rsvp: 'rsvp',
-		pictures: 'pictures',
-		events: 'events',
-		weddingParty: 'wedding-party',
-		travel: 'travel',
-		registry: 'registry'
-	}
+  static uiRoutes = {
+    empty: '',
+    home: 'home',
+    rsvp: 'rsvp',
+    pictures: 'pictures',
+    events: 'events',
+    weddingParty: 'wedding-party',
+    travel: 'travel',
+    registry: 'registry'
+  }
 }
